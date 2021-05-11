047e47860db947a10360c613e717b122ac649383
MathyouMB <matthewmacraebovell@bell.net>
2021-05-11 12:43:57 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Router from 'svelte-spa-router';
+  import {Home, NotFound} from './pages';
+  const routes = {
+    '/': Home,
+    '*': NotFound
+  };
+</script>
+
+<Router {routes} />
---
+++ src/pages/Home/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+  import {Button} from '../../components';
+</script>
+
+<h1>Home</h1>
+<Button label="Test" />
+
+<style type="text/scss">
+  @import 'Home.scss';
+</style>
